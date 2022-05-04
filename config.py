2f5128dbfdcc92bb86875373f68883d89f264c44
tamilvip007 <indrajeethy.it20@scew.org>
2022-05-05 03:33:40 +0530

    kek

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 from os import environ as env
 
-'''
 
 APP_ID = env.get("APP_ID")
 APP_HASH = env.get("APP_HASH")
@@ -11,14 +10,5 @@ OWNER = env.get("OWNER")
 BOT_USERNAME = env.get("BOT_USERNAME")
 MONGO_URI = env.get("MONGO_URI")
 
-'''
 
-MONGO_DB_URL = "mongodb+srv://userbot:userbot@cluster0.tlbcm.mongodb.net/Cluster0?retryWrites=true&w=majority"
-BOT_USERNAME = "@Gamerbotto_bot"
-BOT_TOKEN = "5319223736:AAGByFPbmQkcFh-vAzJyWo7V3bKAttqk2RQ"
-APP_ID = 2992000
-APP_HASH = "235b12e862d71234ea222082052822fd"
-KUKI_TOKEN = "2107137268-KUKIYc9WRc1eN5"
-BOT_NAME = "KUKI"
-OWNER = "tamilvip007"
 
