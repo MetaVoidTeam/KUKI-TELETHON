ccad95fab01ba9fe7c724b213126299ad6d73738
tamilvip007 <indrajeethy.it20@scew.org>
2022-05-09 22:35:16 +0530

    added toggle buttons

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from functools import wraps
 
 from requests import JSONDecodeError, get
-from telethon import TelegramClient, events
+from telethon import TelegramClient, events, Button
 from telethon.errors.rpcerrorlist import UserNotParticipantError
 
 from config import *
@@ -24,6 +24,12 @@ def cmd(**args):
 
     return decorator
 
+
+def cbk(**args):
+    def decorator(func):
+        kuki.add_event_handler(func, events.CallbackQuery(**args))
+        return func
+    return decorator
 
 def ryts(func):
     @wraps(func)
@@ -89,34 +95,39 @@ async def start(e):
     await e.reply("hey, I'm {}, a chatbot for Telegram.\n".format(BOT_NAME))
 
 
-@cmd(pattern="addchat")
+@cmd(pattern="setchat")
 @ryts
-async def addchat(e):
+async def setchat(e):
     if e.is_private:
-        await e.reply("You can't use this command in a private chat!")
+        await e.reply("This command can't be used in private chats.")
         return
+    buttons = Button.inline("Enable", data="enable_{}".format(e.sender_id)), Button.inline("Disable", data="disable_{}".format(e.sender_id))
+    await e.reply("AI chat setup", buttons=buttons)
+
+
+@cbk(pattern="enable_(.*)")
+async def enable_ai(e):
+    user = int(e.pattern_match.group(1))
+    ryts = await kuki.get_permissions(e.chat_id, e.sender_id)
+    if not user == e.sender_id:
+        return await e.answer("You ain't the one who used this command.", alert=True)
     elif Chat.is_ai_chat(e.chat_id):
-        await e.reply("Kuki Ai is already **enabled!**")
-        return
+        return await e.edit("AI is already enabled in this chat.")
+    await e.edit("Successfully enabled Kuki Ai in **{}** by [{}](tg://user?id={})".format(e.chat.title, e.sender.first_name, e.sender_id))
     Chat.add_chat(e.chat_id)
-    await e.reply(
-        f"Kuki Ai enabled by [{e.sender.first_name}](tg://user?id={e.sender_id}) in **{e.chat.title}**"
-    )
 
 
-@cmd(pattern="rmchat")
-@ryts
-async def rmchat(e):
-    if e.is_private:
-        await e.reply("You can't use this command in private chat!")
-        return
+@cbk(pattern="disable_(.*)")
+async def disable_ai(e):
+    user = int(e.pattern_match.group(1))
+    ryts = await kuki.get_permissions(e.chat_id, e.sender_id)
+    if not user == e.sender_id:
+        return await e.answer("You aint the one who used this command.", alert=True)
     elif not Chat.is_ai_chat(e.chat_id):
-        await e.reply("Kuki Ai is already **disabled!**")
-        return
+        return await e.edit("AI is already disabled in this chat.")
+    await e.edit("Successfully disabled Kuki Ai in **{}** by [{}](tg://user?id={})".format(e.chat.title, e.sender.first_name, e.sender_id))
     Chat.rm_chat(e.chat_id)
-    await e.reply(
-        f"Kuki Ai disabled by [{e.sender.first_name}](tg://user?id={e.sender_id}) in **{e.chat.title}**"
-    )
+
 
 
 @kuki.on(
